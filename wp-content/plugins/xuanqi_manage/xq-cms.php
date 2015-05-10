4ea82f38a1977633608c6817d9cff7a9b459fd64
blisskid <yuleirichard@163.com>
2015-05-10 19:00:13 +0800

    hello

--- wp-content/plugins/xuanqi_manage/xq-cms.php
+++ wp-content/plugins/xuanqi_manage/xq-cms.php
@@ -27,9 +27,9 @@ add_action('init', 'add_styles');
 
 //add js
 function add_scripts() {
+	wp_enqueue_script('angularjs', plugins_url('js/angular.min.js', __FILE__));
 	wp_register_script('plugin_script', plugins_url('js/xuanqi.js', __FILE__), array('jquery'), '1.0', true);
 	wp_enqueue_script('plugin_script');
-	wp_enqueue_script('angularjs', plugins_url('js/angular.min.js', __FILE__));
 }
 
 add_action('init', 'add_scripts');
@@ -47,7 +47,7 @@ function register_my_custom_menu_page() {
 
 function xuanqi_products_config_callback() {
 	?>
-<div ng-app="myApp" ng-controller="customersCtrl" class="xqgrid">
+<div ng-app="showProductApp" ng-controller="showProductCtrl" class="xqgrid">
 	<table>
         <th>产品名称</th>
         <th>产品价格（元）</th>
@@ -68,8 +68,8 @@ function xuanqi_products_config_callback() {
     </table>
 </div>
 <script>
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
+var app = angular.module('showProductApp', []);
+app.controller('showProductCtrl', function($scope, $http) {
     $http.get('<?php echo get_bloginfo('wpurl') . "/show-product";?>').success(function(response) {
         $scope.names = response.records;
     });
@@ -80,90 +80,101 @@ app.controller('customersCtrl', function($scope, $http) {
 }
 
 function xuanqi_add_product_callback() {
-	$url = get_bloginfo('wpurl') . "/xuanqi-save-product";
+
+	$url = get_bloginfo('wpurl') . "/save-product";
 	echo "<div class=\"xqform\">";
-	echo "<form action='$url' method='post'>";
+	echo "<form ng-app=\"addProductApp\"  ng-controller=\"addProductCtrl\" name=\"addProductForm\" novalidate>";
 	?>
 
-		<table align="center">
-		    <tr>
-		        <td>
-		            输入产品名称：
-		        </td>
-		        <td>
-		            <input style="width:300px" type="text" value="" id="product_name" name="product_name"></input>
-		            <input type="hidden" id="ID" name="ID" value="">
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		            输入产品价格：
-		        </td>
-		        <td>
-		            <input style="width:300px" type="text" value="" id="product_price" name="product_price">（元）</input>
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		            输入经销商产品价格：
-		        </td>
-		        <td>
-		            <input style="width:300px" type="text" value="" id="product_dealer_price" name="product_dealer_price">（元）</input>
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		            选择产品类别：
-		        </td>
-		        <td>
-					<select style="width:300px" id="product_type" name="product_type">
-					  <option value="0" selected>疫苗类产品</option>
-					  <option value="1">其他产品</option>
-					</select>
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		            选择产品支付流程：
-		        </td>
-		        <td>
-					<select style="width:300px" id="product_paytype" name="product_paytype">
-					  <option value="0" selected>支付流程一</option>
-					  <option value="1">支付流程二</option>
-					</select>
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		            产品是否在首页展示：
-		        </td>
-		        <td>
-					<select style="width:300px" id="product_show" name="product_show">
-					  <option value="0">不在首页显示</option>
-					  <option value="1" selected>在首页显示</option>
-					</select>
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		            输入产品介绍：
-		        </td>
-		    </tr>
-		    <tr>
-		        <td colspan="2">
-		        	<?php
-wp_editor("", "product_description");
-	?>
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		            <input type="submit" value="保存产品"></input>
-		        </td>
-		    </tr>
-		</table>
-	</form>
+<table align="center">
+    <tr>
+        <td>
+        	<input type="hidden" id="product_id" name="product_id" value=""/>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            输入产品名称：
+            <br>
+            <input style="width:300px" type="text" value="" name="product_name" id="product_name" ng-model="product_name" required></input>
+            <span style="color:red" ng-show="addProductForm.product_name.$dirty && addProductForm.product_name.$invalid">
+				 		<span ng-show="addProductForm.product_name.$error.required">请输入产品名称.</span>
+            </span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            输入产品价格：
+            <br>
+            <input style="width:300px" type="number" value="" name="product_price" id="product_price" ng-model="product_price" required>（元）</input>
+            <span style="color:red" ng-show="addProductForm.product_price.$dirty && addProductForm.product_price.$invalid">
+				 		<span ng-show="addProductForm.product_price.$error.required">请输入产品价格.</span>
+            <span ng-show="addProductForm.product_price.$error.number">产品价格需要为数字.</span>
+            </span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            输入经销商产品价格：
+            <br>
+            <input style="width:300px" type="number" value="" name="product_dealer_price" id="product_dealer_price" ng-model="product_dealer_price" required>（元）</input>
+            <span style="color:red" ng-show="addProductForm.product_dealer_price.$dirty && addProductForm.product_dealer_price.$invalid">
+				 		<span ng-show="addProductForm.product_dealer_price.$error.required">请输入经销商产品价格.</span>
+            <span ng-show="addProductForm.product_dealer_price.$error.number">经销商产品价格需要为数字.</span>
+            </span>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            选择产品类别：
+            <br>
+            <select style="width:300px" name="product_type" id="product_type" ng-model="product_type">
+                <option value="0" selected>疫苗类产品</option>
+                <option value="1">其他产品</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            选择产品支付流程：
+            <br>
+            <select style="width:300px" name="product_paytype" id="product_paytype" ng-model="product_paytype">
+                <option value="0" selected>支付流程一</option>
+                <option value="1">支付流程二</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            产品是否在首页展示：
+            <br>
+            <select style="width:300px" name="product_show" id="product_show" ng-model="product_show">
+                <option value="0">不在首页显示</option>
+                <option value="1" selected>在首页显示</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            输入产品介绍：
+            <?php wp_editor("", "description");?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        	<button onclick="saveProduct('<?php echo get_bloginfo('wpurl') . "/save-product";?>')" ng-disabled="addProductForm.product_name.$dirty && addProductForm.product_name.$invalid || addProductForm.product_price.$dirty && addProductForm.product_price.$invalid || addProductForm.product_dealer_price.$dirty && addProductForm.product_dealer_price.$invalid">保存产品</button>
+        	<button ng-click="submit()">保存产品</button>
+         </td>
+    </tr>
+</table>
+</form>
 </div>
+<script>
+var app = angular.module('addProductApp', []);
+app.controller('addProductCtrl', function($scope, $http) {
+
+});
+</script>
 <?php
 
 }
